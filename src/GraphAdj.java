f12f4c60e1a695002d97b1a09c8755a4bff1aef2
leo krebs <leo.krebs@etu.unistra.fr>
2020-01-22 08:57:58 +0100

    Changement sur la javadoc

--- src/GraphAdj.java
+++ src/GraphAdj.java
@@ -1,11 +1,5 @@
 public class GraphAdj implements Graphe {
 
-    /**
-     * Fonction permettant de voir si deux sommets sont voisins.
-     * @param i Le premier sommet a tester.
-     * @param j Le deuxieme sommet a tester.
-     * @return <b>True</b> si les deux sommets sont voisins, <b>False</b> sinon.
-     */
     @Override
     public boolean isVoisin(int i, int j) {
         return false;
--- src/Graphe.java
+++ src/Graphe.java
@@ -3,6 +3,12 @@
  */
 public interface Graphe {
 
+    /**
+     * Fonction permettant de voir si deux sommets sont voisins.
+     * @param i Le premier sommet a tester.
+     * @param j Le deuxieme sommet a tester.
+     * @return <b>True</b> si les deux sommets sont voisins, <b>False</b> sinon.
+     */
     boolean isVoisin(int i, int j);
 
 }
