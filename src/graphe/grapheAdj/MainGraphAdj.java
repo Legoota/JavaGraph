ae69b396780c0492eeba228bdb41b004af6a2ad4
htfDl3iewo1NUAH8upDO <leodu39200@gmail.com>
2020-03-12 18:48:58 +0100

    Modification du graph

--- src/graphe/grapheAdj/MainGraphAdj.java
+++ src/graphe/grapheAdj/MainGraphAdj.java
@@ -12,20 +12,20 @@ public class MainGraphAdj {
      * @param args Arguments du main
      */
     public static void main(String[] args){
-        int[][] mat =  {{0,1,1,0,1,1,0},
-                        {1,0,0,0,1,1,0},
+        int[][] mat =  {{1,1,1,0,1,1,0},
+                        {1,1,0,1,0,0,0},
+                        {1,0,1,0,0,0,0},
+                        {0,1,0,1,0,1,1},
                         {1,0,0,0,1,0,0},
-                        {0,0,0,0,1,0,0},
-                        {1,1,1,1,0,0,0},
-                        {1,1,0,0,0,0,1},
-                        {0,0,0,0,0,1,0}};
+                        {1,0,0,1,0,1,1},
+                        {0,0,0,1,0,1,1}};
         Matrice m = new Matrice(mat);
         GraphAdj graphe = new GraphAdj(m);
         //TODO: au choix pour les diagonales
 
         System.out.println(graphe.isVoisin(2,3));// Test de la fonction isVoisin (cas NOK)
-        System.out.println(graphe.isVoisin(4,1));// Test de la fonction isVoisin (cas OK)
+        System.out.println(graphe.isVoisin(4,0));// Test de la fonction isVoisin (cas OK)
         System.out.println(graphe.distance(1,3));// Test de la fonction distance
-        System.out.println(graphe.distance(3,1));// Test de la fonction distance
+        System.out.println(graphe.distance(6,0));// Test de la fonction distance
     }
 }
