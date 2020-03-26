d79fcb64100571212083e01740e5fa6d6fc8669a
htfDl3iewo1NUAH8upDO <leodu39200@gmail.com>
2020-03-26 13:35:03 +0100

    Regénération de la JavaDoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"graphe"},{"l":"graphe.grapheAdj"},{"l":"graphe.grapheS"},{"l":"matrice"},{"l":"tests"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"exceptions"},{"l":"graphe"},{"l":"graphe.grapheAdj"},{"l":"graphe.grapheS"},{"l":"matrice"},{"l":"tests"}]
