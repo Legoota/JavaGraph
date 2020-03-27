e0bb7f2aa05b767f8609723be103fb419ce16a01
Léo Krebs <leokrebs68@gmail.com>
2020-03-27 11:32:40 +0100

    Ajout test Sommet + javadoc tests

--- src/tests/GraphSTest.java
+++ src/tests/GraphSTest.java
@@ -12,6 +12,9 @@ import static org.junit.Assert.*;
 public class GraphSTest {
     GraphS graphe;
 
+    /**
+     * Initialisation d'un graphe de test
+     */
     @Before
     public void setUp() {
         graphe = new GraphS(6);
@@ -22,47 +25,74 @@ public class GraphSTest {
         graphe.addArete(4,0);
     }
 
+    /**
+     * Test d'ajout d'une valeur negative (doit renvoyer une erreur)
+     */
     @Test (expected = IllegalArgumentException.class)
     public void isVoisinNegatif() {
         graphe.isVoisin(-1, 1);
         fail("Aurait du lancer IllegalArgumentException");
     }
 
+    /**
+     * Test de presence d'une arete entre deux sommets
+     */
     @Test
     public void isVoisinOk() {
         assertEquals(true, graphe.isVoisin(0,1));
     }
 
+    /**
+     * Test de non presence d'une arete entre deux sommets
+     */
     @Test
     public void isVoisinNotOk() {
         assertEquals(false, graphe.isVoisin(0,3));
     }
 
+    /**
+     * Test d'ajout d'une arete avec un sommet negatif
+     */
     @Test (expected = IllegalArgumentException.class)
     public void addAreteNegatif() {
-        graphe.addArete(-1,-1);
+        graphe.addArete(-1,1);
     }
 
+    /**
+     * Test d'ajout d'une arete entre deux sommets (valide)
+     */
     @Test
     public void addAreteOk() {
         assertEquals(true,(graphe.isVoisin(0,1)));
     }
 
+    /**
+     * Test d'ajout d'une arete entre deux sommets (non valide)
+     */
     @Test
     public void addAreteNotOk() {
         assertEquals(false, graphe.isVoisin(0,3));
     }
 
+    /**
+     * Test de distance entre deux sommets (valide)
+     */
     @Test
     public void distanceOk() {
         assertEquals(3,graphe.distance(3,4));
     }
 
+    /**
+     * Test de distance entre deux sommets qui n'ont pas de chemin (non valide)
+     */
     @Test
     public void distanceNotOk() {
         assertEquals(-1,graphe.distance(5,0));
     }
 
+    /**
+     * Test de distance avec un sommet inexistant (doit rendre une erreur)
+     */
     @Test (expected = IllegalArgumentException.class)
     public void distanceError() {
         graphe.distance(-1,1);
--- src/tests/SommetTest.java
+++ src/tests/SommetTest.java
@@ -17,30 +17,68 @@ public class SommetTest {
     private Sommet s4;
     private Sommet s5;
 
+    /**
+     * Initialisation de 5 sommets
+     */
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
 
-        Sommet s1 = new Sommet(1);
-        Sommet s2 = new Sommet(2);
-        Sommet s3 = new Sommet(3);
-        Sommet s4 = new Sommet(4);
-        Sommet s5 = new Sommet(5);
+        s1 = new Sommet(1);
+        s2 = new Sommet(2);
+        s3 = new Sommet(3);
+        s4 = new Sommet(4);
+        s5 = new Sommet(5);
     }
 
+    /**
+     * Test ajout d'un voisin entre s1 et s2 (valide)
+     */
     @Test
     public void ajoutVoisinOk() {
         s1.addVoisin(s2);
         assertEquals(true, s1.isVoisin(s2));
     }
 
+    /**
+     * Test de verification de la presence d'une arete entre s1 et s2 (valide)
+     */
     @Test
     public void isVoisinOk() {
         s1.addVoisin(s2);
-        assertEquals(true, s1.isVoisin(s2));
+        assertEquals(true, s1.getVoisins().contains(s2));
     }
 
+    /**
+     * Test de verification de la non presence d'une arete entre s1 et s3
+     */
     @Test
     public void isVoisinNotOk() {
-        assertEquals(false, s1.isVoisin(s3));
+        assertEquals(false, s1.getVoisins().contains(s3));
+    }
+
+    /**
+     * Test du flag correctement initialise
+     */
+    @Test
+    public void getFlagOk() {
+        assertEquals(false, s4.getFlag());
+    }
+
+    /**
+     * Test du setter de flag (valide)
+     */
+    @Test
+    public void setFlagOk() {
+        s5.setFlag(true);
+        assertEquals(true, s5.getFlag());
+    }
+
+    /**
+     * Test de l'impossibilite de creer un sommet avec id negatif (doit renvoyer une erreur)
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void sommetConstructorNotOk() {
+        new Sommet(-1);
+        fail("N'aurait pas du creer un sommet d'id negatif");
     }
 }
