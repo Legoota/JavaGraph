13a8282a1c820d330b6d641120bd78dd608d2472
Léo Krebs <leokrebs68@gmail.com>
2020-05-03 13:36:41 +0200

    Visualizer.js refactor
    From anonymous functions to arrow functions

--- graphVisualizer/visualizer/visualizer.js
+++ graphVisualizer/visualizer/visualizer.js
@@ -1,3 +1,4 @@
+// NEIGHBORS METHOD
 sigma.classes.graph.addMethod('neighbors', function(nodeId) {
     var k,
         neighbors = {},
@@ -7,9 +8,10 @@ sigma.classes.graph.addMethod('neighbors', function(nodeId) {
       neighbors[k] = this.nodesIndex[k];
 
     return neighbors;
-  });
+ });
+ // END OF NEIGHBORS METHOD
 
-
+// SIGMA INSTANCE INITIALIZATION
 var g = {
         nodes: [],
         edges: []
@@ -27,15 +29,17 @@ var s = new sigma({
         maxNodeSize: 16
     }
 });
+// END OF SIGMA INSTANCE INITIALIZATION
 
+// SIGMA JSON IMPORT PART
 sigma.parsers.json(
     'data/reseau.json',
     s,
     function() {
         // this is needed in case the original JSON doesn't have color / size / x-y attributes
         var i,
-                nodes = s.graph.nodes(),
-                len = nodes.length;
+            nodes = s.graph.nodes(),
+            len = nodes.length;
 
         for (i = 0; i < len; i++) {
             nodes[i].x = Math.random();
@@ -43,68 +47,40 @@ sigma.parsers.json(
             nodes[i].size = s.graph.degree(nodes[i].id);
             nodes[i].color = nodes[i].center ? '#ff4b19' : '#ff4b19';
         }
-
-        // Refresh the display:
         s.refresh();
 
-        // ForceAtlas Layout
-        s.startForceAtlas2({worker: true, barnesHutOptimize: false});
-        setTimeout(() => {  s.stopForceAtlas2(); }, 1);
-    }
-);
+        // ForceAtlas spacing algorithm
+        s.startForceAtlas2({worker: true, barnesHutOptimize: true});
+        setTimeout(() => {  s.stopForceAtlas2(); }, 1); // Stop FA 1ms afterwards (or else it keeps moving)
+});
+// END OF SIGMA JSON IMPORT PART
 
-// We first need to save the original colors of our
-  // nodes and edges, like this:
-  s.graph.nodes().forEach(function(n) {
-    n.originalColor = n.color;
-    console.log(n.originalColor);
-  });
-  s.graph.edges().forEach(function(e) {
-    e.originalColor = e.color;
-  });
+// NEIGHBOR COLORING PART
+// Saving original nodes & edges colors
+  s.graph.nodes().forEach(n => n.originalColor = n.color);
+  s.graph.edges().forEach(e => e.originalColor = e.color);
 
-  // When a node is clicked, we check for each node
-  // if it is a neighbor of the clicked one. If not,
-  // we set its color as grey, and else, it takes its
-  // original color.
-  // We do the same for the edges, and we only keep
-  // edges that have both extremities colored.
+  // CLICK A NODE NEIGHBORS COLORING METHOD
   s.bind('clickNode', function(e) {
     var nodeId = e.data.node.id,
         toKeep = s.graph.neighbors(nodeId);
-    toKeep[nodeId] = e.data.node;
+    toKeep[nodeId] = e.data.node; // toKeep contains all the neighbouring nodes
 
-    s.graph.nodes().forEach(function(n) {
-      if (toKeep[n.id])
-        n.color = '#ff4b19';
-      else
-        n.color = '#666';
-    });
-
-    s.graph.edges().forEach(function(e) {
-      if (toKeep[e.source] && toKeep[e.target])
-        e.color = '#ff4b19';
-      else
-        e.color = '#666';
-    });
-
-    // Since the data has been modified, we need to
-    // call the refresh method to make the colors
-    // update effective.
+    // Change node color to gray if not in toKeep, orange if in toKeep
+    s.graph.nodes().forEach(n => toKeep[n.id] ? n.color = '#ff4b19' : n.color = '#666');
+    // Change edge color to gray if not in toKeep, orange if in toKeep
+    s.graph.edges().forEach(e => toKeep[e.source] && toKeep[e.target] ? e.color = '#ff4b19' : e.color = '#666');
+    // Refresh to get updated colors
     s.refresh();
   });
 
-  // When the stage is clicked, we just color each
-  // node and edge with its original color.
+  // CLICK THE BACKGROUND COLORING METHOD
   s.bind('clickStage', function(e) {
-    s.graph.nodes().forEach(function(n) {
-      n.color = '#ff4b19';
-    });
-
-    s.graph.edges().forEach(function(e) {
-      e.color = '#ff4b19';
-    });
-
-    // Same as in the previous event:
+    // Reset all nodes to same color
+    s.graph.nodes().forEach(n => n.color = '#ff4b19');
+    // Reset all edges to same color
+    s.graph.edges().forEach(e => e.color = '#ff4b19');
+    // Refresh to get updated colors
     s.refresh();
   });
+// END OF NEIGHBOR COLORING PART
