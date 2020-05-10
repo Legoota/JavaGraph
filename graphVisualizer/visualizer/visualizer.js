783938e51f3d42378869eadb12539646273f7799
Léo Krebs <leokrebs68@gmail.com>
2020-05-10 18:28:31 +0200

    Update thème visualizer color

--- graphVisualizer/visualizer/visualizer.js
+++ graphVisualizer/visualizer/visualizer.js
@@ -26,40 +26,35 @@ var s = new sigma({
     },
     settings: {
         minNodeSize: 8,
-        maxNodeSize: 16
+        maxNodeSize: 24
     }
 });
 // END OF SIGMA INSTANCE INITIALIZATION
 
 // SIGMA JSON IMPORT PART
 sigma.parsers.json(
-    'data/reseau.json',
+    'data/grex.json',
     s,
     function() {
         // this is needed in case the original JSON doesn't have color / size / x-y attributes
-        var i,
-            nodes = s.graph.nodes(),
+        var nodes = s.graph.nodes(),
             len = nodes.length;
 
-        for (i = 0; i < len; i++) {
+        for (var i = 0; i < len; i++) {
             nodes[i].x = Math.random();
             nodes[i].y = Math.random();
             nodes[i].size = s.graph.degree(nodes[i].id);
-            nodes[i].color = nodes[i].center ? '#ff4b19' : '#ff4b19';
+            nodes[i].color = '#7c9cb1'
         }
         s.refresh();
 
         // ForceAtlas spacing algorithm
         s.startForceAtlas2({worker: true, barnesHutOptimize: true});
-        setTimeout(() => {  s.stopForceAtlas2(); }, 1); // Stop FA 1ms afterwards (or else it keeps moving)
+        setTimeout(() => {  s.stopForceAtlas2(); }, 1000); // Stop FA 1ms afterwards (or else it keeps moving)
 });
 // END OF SIGMA JSON IMPORT PART
 
 // NEIGHBOR COLORING PART
-// Saving original nodes & edges colors
-  s.graph.nodes().forEach(n => n.originalColor = n.color);
-  s.graph.edges().forEach(e => e.originalColor = e.color);
-
   // CLICK A NODE NEIGHBORS COLORING METHOD
   s.bind('clickNode', function(e) {
     var nodeId = e.data.node.id,
@@ -67,9 +62,9 @@ sigma.parsers.json(
     toKeep[nodeId] = e.data.node; // toKeep contains all the neighbouring nodes
 
     // Change node color to gray if not in toKeep, orange if in toKeep
-    s.graph.nodes().forEach(n => toKeep[n.id] ? n.color = '#ff4b19' : n.color = '#666');
+    s.graph.nodes().forEach(n => toKeep[n.id] ? n.color = '#7c9cb1' : n.color = '#666');
     // Change edge color to gray if not in toKeep, orange if in toKeep
-    s.graph.edges().forEach(e => toKeep[e.source] && toKeep[e.target] ? e.color = '#ff4b19' : e.color = '#666');
+    s.graph.edges().forEach(e => toKeep[e.source] && toKeep[e.target] ? e.color = '#547989' : e.color = '#666');
     // Refresh to get updated colors
     s.refresh();
   });
@@ -77,9 +72,9 @@ sigma.parsers.json(
   // CLICK THE BACKGROUND COLORING METHOD
   s.bind('clickStage', function(e) {
     // Reset all nodes to same color
-    s.graph.nodes().forEach(n => n.color = '#ff4b19');
+    s.graph.nodes().forEach(n => n.color = '#7c9cb1');
     // Reset all edges to same color
-    s.graph.edges().forEach(e => e.color = '#ff4b19');
+    s.graph.edges().forEach(e => e.color = '#547989');
     // Refresh to get updated colors
     s.refresh();
   });
