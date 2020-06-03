71fb72534b807798f2e90cd0780ceb5fa7b70b5d
htfDl3iewo1NUAH8upDO <leodu39200@gmail.com>
2020-06-03 18:12:41 +0200

    Ajout Main Routeur

--- src/graphe/grapheS/reseau/MainRouteur.java
+++ src/graphe/grapheS/reseau/MainRouteur.java
@@ -12,65 +12,41 @@ public class MainRouteur {
      * @param args Arguments du Main
      */
     public static void main(String[] args) {
-        try {
-            /*int size = 500;
-            GraphS g = new GraphS(size);
-            Routeur R = new Routeur(size, new Ip(110, 23, 12, 12), 20);
-            for (int i = 0; i < size - 2; i++) {
-                g.addArete(i, i + 1);
-                R.addArete(i, i + 1);
-                g.addArete(i, i + 2);
-                R.addArete(i, i + 2);
-                //G.addArete(i,i+10);GA.addArete(i,i+10);
-            }
-            g.addArete(0, 5);
-            R.addArete(0, 5);
-            //System.out.println(g);
-            GraphS bfsG = g.BFS();
-            //System.out.println(bfsG);
-            GraphS dfsG = g.DFS();
-            //System.out.println(dfsG);
-            long tdebut = System.currentTimeMillis();
-            int d = g.distance(0, 300);
-            long tfin = System.currentTimeMillis();
-            R.DHCP();
-            //System.out.println(d + " en " + (tfin - tdebut) + " ms.");
-            //System.out.println(R.getMachineByIp(new Ip(110, 23, 0, 10)).getId());
-            //System.out.println(R.getMachineByIp(new Ip(110, 23, 1, 10)).getId());
-            System.out.println("Routeur : "+R.getReseau());
-            System.out.println("Broadcast : "+R.getBroadcast());
-            System.out.println("@IP PC id 100 : "+R.getMachineById(100).getIp());
-            System.out.println("@ID PC ip 110.23.1.10 : "+R.getMachineByIp(new Ip(110, 23, 1, 10)).getId());
-            //Export.exportToJson(R,"reseau");*/
+        System.out.println("Main classe Routeur");
+        System.out.println("Ajout d'un routeur d'ip externe 1.1.1.1, contenant 10 machines d'ip 192.168.1.X, masque 24");
+        Routeur routeur = new Routeur(1,10,new Ip(1,1,1,1),new Ip(192,168,1,1),24);
+        System.out.println("Affichage de "+routeur.toString());
 
-        } catch (BadSizeGrapheException e) {
-            System.out.println(e);
-        }
+        System.out.println("L'adresse du routeur est : "+routeur.getReseau());
+        System.out.println("L'adresse de broadcast est : "+routeur.getBroadcast());
+        System.out.println("Le masque est :"+routeur.getMasque()+"\n");
 
+        System.out.println("Appel de la methode DHCP");
+        routeur.DHCP();//renseigne le adresses ip des machines connectees au routeur
+        System.out.println(routeur.toString());
 
-        /*Ip machine = new Ip(192, 168, 1, 1);
-        Ip masque = new Ip(255, 255, 0, 0);
-        System.out.print("Le reseau de la machine "+machine+" (masque : "+masque+") est : ");*/
+        Machine machine = new Machine(100,Machine.Type.PC);
+        routeur.addMachine(machine);
+        System.out.println("Ajout de la machine d'id "+machine.getId());
+        System.out.println(routeur.toString());
 
-        Routeur R = new Routeur(1,10,new Ip(1,1,1,1),new Ip(192,168,1,1),5);
-        System.out.println("@Routeur: "+R.getReseau());
-        System.out.println("@Broad: "+R.getBroadcast());
-        System.out.println("@Masque: "+R.getMasque());
-        R.DHCP();
+        System.out.println("Demmande de reattribution d'adresse ip pour la machine d'id "+machine.getId());
+        machine.DHCPM(routeur);//donne la premiere adresse ip disponible
+        System.out.println("Nouvelle adresse ip de la machine d'id "+machine.getId()+" : "+machine.getIp());
 
-        Machine m = new Machine(15,Machine.Type.PC);
-        Machine m2 = new Machine(20,Machine.Type.PC);
-        R.addMachine(m);
-        R.addMachine(m2);
-        System.out.println("NOUVEAU RESEAU: "+R.toString());
-        m.DHCPM(R);
-        m2.DHCPM(R);
-        System.out.println(m2.getIp().toString());
+        System.out.println("\nMesssagerie");
+        System.out.println("Envoi d'un message de la machine d'adresse "+machine.getIp().toString()+" a la machine d'ip "+routeur.getMachineById(1).getIp().toString());
+        machine.sendMessage("Bonjour, voici mon message ! ;) ",routeur.getMachineById(1).getIp(),routeur);//envoi d'un message de la machine "machine" a la machine d'id 1
+        System.out.println("Recuperation du message sur la machine d'ip "+ routeur.getMachineById(1).getIp()+" : "+routeur.getMachineById(1).getMessages()+"\n");
 
-        m.sendMessage("Test venant de M",R.getMachineById(20).getIp(),R);
-        System.out.println(m2.getMessagesFromIp(new Ip(192,0,0,13)));
-        m2.printLogs();
-        m.sendMessage("Deuxieme test venant de M",R.getMachineById(20).getIp(),R);
-        m2.printMessages();;
+        System.out.println("Affichage des logs de la machine \n"+machine.getIp().toString());
+        machine.printLogs();
+
+        System.out.println("\nSuppression de la machine d'id "+machine.getId());
+        routeur.deleteMachine(machine);//suppression d'une machine
+        System.out.println("Suppression de la machine d'ip "+routeur.getMachineById(5).getIp());
+        routeur.deleteMachine(routeur.getMachineByIp(new Ip(192,168,1,6)).getId());//suppression d'une machine par son id
+        System.out.println("\nNouvel affichage des machine du routeur");
+        System.out.println(routeur.toString());
     }
 }
